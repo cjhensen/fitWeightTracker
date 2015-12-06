803c347bb895db950499d0c1b2f9db2f543072be
Christian Hensen <christian.j.hensen@gmail.com>
2015-12-06 17:13:12 -0500

    using gulp to bundle bower components

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,19 @@ var concat = require('gulp-concat');
 
 // sass
 var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
 
 // js
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
-
-//var browserify = require('browserify');
+var mainBowerFiles = require('main-bower-files');
+var order = require('gulp-order');
 
 
 // gulp defaults
 gulp.task('default', ['connect', 'clean', 'watch'], function() {
-	gulp.start('sass', 'html', 'js');
+	gulp.start('sass', 'html', 'vendor', 'js');
 });
 
 // watch livereload
@@ -48,6 +49,7 @@ gulp.task('connect', function() {
 gulp.task('sass', function() {
 	return gulp.src('./src/app/sass/app.scss')
 	.pipe(sass().on('error', sass.logError))
+	.pipe(minifycss())
 	.pipe(gulp.dest('dev_build/css'))
 	.pipe(notify({ message: 'SASS compiled successfully' }));
 });
@@ -63,7 +65,7 @@ gulp.task('html', function() {
 gulp.task('js', function() {
 	return gulp.src(['./src/app/app.js', './src/app/**/*.js'])
 	.pipe(sourcemaps.init())
-		.pipe(concat('app.bundle.js'))
+		.pipe(concat('app.bundle.min.js'))
 		.pipe(ngAnnotate()) // adds angular dependency injection annotations
 		.pipe(uglify())
 	.pipe(sourcemaps.write())
@@ -71,8 +73,24 @@ gulp.task('js', function() {
 	.pipe(notify({ message: 'JS compiled successfully' }));
 });
 
+// vendor files
+gulp.task('vendor', function(){  
+  return gulp.src(mainBowerFiles({
+  	paths: {
+  		bowerJson: 'bower.json',
+			bowerDirectory: './bower_components'
+  	}
+  }))
+  .pipe(order(["*jquery*", "*angular*", "*bootstrap-sass*"]))
+  .pipe(concat('vendor.bundle.min.js'))
+  .pipe(uglify().on('error', function(e) {
+  	console.log('\x07',e.message); return this.end(); 
+  }))
+  .pipe(gulp.dest('./dev_build/js'))
+  .pipe(notify({ message: 'Vendor JS files compiled successfully' }));
+});
 
 // flush dist folders
 gulp.task('clean', function () {
-	return del(['dev_build/css']);
+	return del(['!dev_build/favicon.ico', 'dev_build/**/*']);
 });
